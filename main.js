21bfc9452001d38c29d45c94610e629a51036be7
Ripal Patel <ripal212007@gmail.com>
2018-09-01 12:17:55 -0500

    added js file

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log('Hi');
+
+const name = "Fred"
+
+if (name === "Fred") {
+   console.log("Hello Fred")
+}
+
+const drinkingAge = 21
+const age = 35
+
+if (age < drinkingAge) {
+    console.log("You can't drink here")
+} else {
+    console.log("Have a beer")
+}
+
+const colors = ["Red", "Orange", "Periwinkle", "Goldenrod", "Tomato", "Dodger Blue"]
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
